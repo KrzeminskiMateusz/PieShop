1b9d3d75c8d0e1db110af4ad59c407cd3b377283
mateusz <mateusztomaszkrzeminski@gmail.com>
2020-04-20 15:41:48 +0200

    First commit

---
+++ PieShop/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PieShop.Model;
+using PieShop.ViewModels;
+
+namespace PieShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+        public IActionResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                PiesOfTheWeek = _pieRepository.PiesOFTheWeek
+            };
+
+            return View(homeViewModel);
+
+        }
+    }
+}
